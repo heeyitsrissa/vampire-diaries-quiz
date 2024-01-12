43d506d640289ed2ae3060eb7dbccd8b1a4f6586
Marissa Pratt <131078835+heeyitsrissa@users.noreply.github.com>
2024-01-11 22:53:29 -0500

    added questions

---
+++ assets/js/script.js
@@ -0,0 +1,62 @@
+// set attribute and variables 
+
+
+
+
+
+
+
+
+
+const myQuestions = [{
+
+    question: `What year were Stephan and Damon turned?`,
+    answer: {
+        a:'1999',
+        b:'1864',
+        c:'1763',
+        d:'1964'
+    },
+    correct: 'b'
+},
+{
+    question: 'How many times does Bonnie die?',
+    answer: {
+        a:'1',
+        b:'never',
+        c:'2',
+        d:'3'
+    },
+    correct:'c'
+},
+{
+    question:'Who does Elena end up with?',
+    answer: {
+        a:'Matt',
+        b:'Stephan',
+        c:'Klaus',
+        d:'None of the above'
+    },
+    correct:'d'
+},
+{
+    question:'Who does Klaus possess while waiting for his body to get Mystic Falls?',
+    answer: {
+        a:'Alaric',
+        b:'Stephan',
+        c:'Jeremy',
+        d:'Vicky'
+    },
+    correct:'a'
+},
+{
+    question:`Who was Alaric Saltzman's wife?`,
+    answer: {
+        a:`Aunt Jenna`,
+        b:`Elena's Birth mother`,
+        c:`the history teacher`,
+        d:`Matt and Vicky's Mom`
+    },
+    correct: `b`
+}
+];
